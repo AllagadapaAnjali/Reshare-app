a95fa9b16a1fa663f3ebe4c2165fc6358fe82dcd
Anjali <anjali@example.com>
2025-09-21 12:57:12 +0530

    Added render public link to signup1.jsx

--- client/src/pages/Signup1.jsx
+++ client/src/pages/Signup1.jsx
@@ -64,7 +64,9 @@ const Signup = () => {
     setIsLoading(true);
     
     try {
-      const res = await fetch(`http://localhost:5000/api/auth/signup`, {
+      // const res = await fetch(`http://localhost:5000/api/auth/signup`,
+      const res = await fetch(`https://reshare-backend-s5na.onrender.com/api/auth/signup`,
+       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
